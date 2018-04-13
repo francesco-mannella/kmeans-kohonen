15645c99fa52e7e438f5080442cd66122c81a188
Francesco Mannella <francesco.mannella@gmail.com>
2018-04-13 11:26:01 +0200

    modulated

--- modulated_kohonen.py
+++ modulated_kohonen.py
@@ -41,16 +41,18 @@ class SOM(object):
         
         # input vector and weights 
         self.x = tf.placeholder(tf.float32, (batch_num, self.input_channels))
-        # deviation of the neighborhood
-        self.deviation = tf.placeholder(tf.float32, ())
-        self.curr_deviation = 0.7
         # learning_rate
         self.learning_rate = tf.placeholder(tf.float32, ())
         # weights
-        self.W = tf.get_variable("W", (self.input_channels, output_channels), 
-            initializer=tf.random_normal_initializer(stddev=1))
-        
-        self.phis = self.get_phis(sigma=self.deviation)
+        self.W = tf.get_variable("som_W", (self.input_channels, output_channels), 
+            initializer=tf.random_normal_initializer(stddev=0.05))
+        # modulation
+        self.prob = tf.placeholder(tf.float32, [1, self.output_channels])
+        self.out_means = tf.placeholder(tf.float32, (None,2))
+        # generate items 
+        self.out_deviation = tf.placeholder(tf.float32, ()) 
+
+        self.phis = self.get_phis(sigma=(1 - self.prob))
 
         self.graph_model()
 
@@ -65,19 +67,25 @@ class SOM(object):
         #   W(m,output_channels)->(n,m,output_channels)
         xrep = tf.stack([self.x for j in range(self.output_channels)], 2) 
         wrep = tf.stack([self.W for j in range(self.batch_num)], 0) 
-        
+        self.uncertainty = 1.0 - self.prob
+        self.uncert_mean = tf.reduce_mean(self.uncertainty)
+
         # distances of inputs to weights
         o = xrep - wrep
-        norms =tf.norm(o, axis=1)
+        self.norms =tf.norm(o, axis=1)
 
         # for each pattern a vector indicating a gaussian around the winner prototipe
-        rk = tf.argmin(norms, axis=1)
- 
+        rk = tf.argmin(self.norms, axis=1)
+        phi_rk = tf.gather(tf.multiply(self.phis, self.uncertainty)*self.uncert_mean, rk)
+
         # the cost function is the sum of the distances from the winner prototipes
-        self.loss = tf.reduce_sum(tf.multiply(tf.pow(norms, 2), tf.gather(self.phis, rk)))
+        # plus a further context modulation
+        self.loss = tf.reduce_sum(tf.multiply(tf.pow(self.norms, 2), 
+            tf.multiply(phi_rk, self.uncertainty)))
 
         # gradient descent
-        self.train = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+        self.train = tf.train.AdamOptimizer(self.learning_rate).minimize(
+                self.loss, var_list=[self.W])
 
         # generate
         self.out_means = tf.placeholder(tf.float32, (None,2))
@@ -88,24 +96,22 @@ class SOM(object):
         self.out = tf.divide(self.out,tf.expand_dims(self.out_sum, 1))
         self.x_sampled = tf.matmul(self.out, tf.transpose(self.W))
      
-    def train_step(self, batch, dev , lr,  session):
+    def train_step(self, batch, lr, prob, session):
         """
         A single batch of computations for optimization
 
         :param batch: a tensor with the current input patterns
-        :param dev: current standard deviation of the neighborhood
-                distribution in the output layer
         :param lr: current learning rate
         
         :returns: current loss value
         """
         
-        loss_, phis_, _ = session.run([self.loss, self.phis, self.train],
+        norms_, loss_, _ = session.run([self.norms, self.loss, self.train],
                 feed_dict={
                     self.x: batch, 
-                    self.deviation: dev, 
-                    self.learning_rate: lr})
-        return loss_
+                    self.learning_rate: lr,
+                    self.prob: prob})
+        return norms_, loss_
     
     def generative_step(self, means, session):
         """
@@ -117,6 +123,7 @@ class SOM(object):
         :returns: a tensor of generated patterns
 
         """
+
         generated_patterns = session.run(self.x_sampled, 
                 feed_dict={self.out_means: means, 
                     self.out_deviation: self.curr_deviation})
@@ -134,7 +141,7 @@ class SOM(object):
         """
 
         x = tf.range(self.out_side, dtype=tf.float32)
-        X, Y = tf.meshgrid(x,x)
+        Y, X = tf.meshgrid(x,x)
         centroids = tf.transpose(tf.stack([tf_flatten(X), tf_flatten(Y)]))
         if means is None:
             means = tf.identity(centroids)
